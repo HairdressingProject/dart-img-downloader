abc1185528a187639fe7fe47c78ff3e4578cf81d
Diego C <diego.cc.github@gmail.com>
2020-10-06 20:46:48 +0800

    added -f and -d options

--- README.md
+++ README.md
@@ -6,15 +6,25 @@ A sample Dart CLI application that downloads images from the web.
 
 ```bash
 dart bin/main.dart
+dart bin/main.dart help down
 ```
 
 ### Download an image:
+The command below will download [this image](https://upload.wikimedia.org/wikipedia/commons/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg "Sample image") to a directory named `downloads/` in your current working directory, with the filename `img.jpg`.
+
 ```bash
 dart bin/main.dart down https://upload.wikimedia.org/wikipedia/commons/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg
 ```
 
+You may also specify a directory (absolute path) or filename (without extension), using the `--directory (-d)` or `--filename (-f)` options, respectively.
+
+Example:
+
+```bash
+dart bin/main.dart down https://upload.wikimedia.org/wikipedia/commons/d/d1/Mount_Everest_as_seen_from_Drukair2_PLW_edit.jpg -f mount_everest -d "C:/Users/user/Pictures"
+```
+
 ## Known limitations and issues
-- User is not able to specify a download directory path
 - Network errors such as 404 and 403 are not properly handled
 - `-v` (verbose) flag has not yet been implemented
 - No unit tests
--- lib/download_cmd.dart
+++ lib/download_cmd.dart
@@ -9,17 +9,34 @@ class DownloadCommand extends Command {
   @override
   final description = 'Download an image based on the provided URL';
 
+  Directory _downloadDir = Directory(Directory.current.path +
+      (Platform.isWindows ? '\\downloads' : '/downloads'));
+
+  String _filenameWithoutExtension = 'img';
+
   DownloadCommand() {
     argParser.addFlag('verbose',
         abbr: 'v', defaultsTo: false, help: 'Display progress status');
+
+    argParser.addOption('directory',
+        abbr: 'd',
+        defaultsTo: _downloadDir.absolute.path,
+        help: 'Absolute directory path to save the image');
+
+    argParser.addOption('filename',
+        abbr: 'f',
+        defaultsTo: _filenameWithoutExtension,
+        help: 'Filename of the image to be saved, without extension');
   }
 
   @override
   Future<void> run() async {
     var url = argResults.arguments[0];
+    String directory = argResults['directory'];
+    String filename = argResults['filename'];
 
     // check URL provided
-    var exp = RegExp(r'http(s?)\:\/\/(.*)\/(.*)\.(jpg|jpeg|png|gif|bmp)',
+    var exp = RegExp(r'http(s?)\:\/\/(.*)\/(.*)\.(jpg|jpeg|png|gif|bmp|svg)',
         caseSensitive: false, multiLine: true, dotAll: true);
 
     if (exp.hasMatch(url)) {
@@ -29,31 +46,30 @@ class DownloadCommand extends Command {
       final matchEnd = match.end;
 
       final matchedUrl = url.substring(matchStart, matchEnd);
-      final filename = match.group(match.groupCount - 1);
+      // final urlFilename = match.group(match.groupCount - 1);
       final ext = match.group(match.groupCount);
+
       print('\nAttempting to download: ' + matchedUrl);
 
-      var client = HttpClient();
-
-      // check platform and download directory, create one if needed
-      // improvements: allow user to specify a directory
-      final isWindows = Platform.isWindows;
-
-      final downloadDir = Directory(
-          Directory.current.path + (isWindows ? '\\downloads' : '/downloads'));
-
-      if (!await downloadDir.exists()) {
-        await downloadDir.create();
+      if (directory.toString() != _downloadDir.absolute.path) {
+        _downloadDir = Directory(directory);
       }
 
-      final downloadPath = downloadDir.absolute.path +
-          (isWindows ? '\\' : '/') +
+      if (!await _downloadDir.exists()) {
+        await _downloadDir.create();
+      }
+
+      _filenameWithoutExtension = filename;
+
+      final downloadPath = _downloadDir.absolute.path +
+          (Platform.isWindows ? '\\' : '/') +
           filename +
           '.' +
           ext;
 
       // download image and write to downloadPath
       // improvements: handle network errors, 404, 403, etc.
+      var client = HttpClient();
       await client
           .getUrl(Uri.parse(matchedUrl))
           .then((req) => req.close())
